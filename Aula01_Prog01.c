86f47e110b96d3aab357baf80a3b9c67627afe6c
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-01 09:44:18 -0200

    Update Aula01_Prog01.c

--- Aula01_Prog01.c
+++ Aula01_Prog01.c
@@ -11,13 +11,14 @@ Antes de rodar o código, ela recebe uma biblioteca e suas
 funções a partir de um arquivo tipo .h (header ou cabeçalho)
 
 Por padrão, C não inclui nenhuma biblioteca. Mas, dependendo do
-compilador, a stdio pode já vir inclusa em um novo programa*/
+compilador, a stdio ("Standard Input and Output", Entrada e Saída Padrão)
+pode já vir inclusa em um novo programa.*/
 
 //A função main() é necessária para qualquer programa (mas não para todo código C)
 //OBS: A Linguagem C é Case Sensitive, ou seja, diferencia maiúsculas e minúsculas
 void main(void) //Nossa função main() não tem argumentos e retorna "void"
 {
     printf("Hello Cruel World...\n");
-    /*A função printf recebe uma "string"
-    (cadeia de caracteres) como argumento e exibe-a*/
+    /*A função printf recebe uma "string" (cadeia de caracteres) como argumento
+    e exibe-a. O nome vem de "print", imprimir (na tela), e o "f" de formatado.*/
 }
