dff6f5819c9a938c8bc55443c93e944bc7aa4ddf
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-01 09:25:32 -0200

    Update Aula01_Prog02.c

--- Aula01_Prog02.c
+++ Aula01_Prog02.c
@@ -1,30 +1,32 @@
 /*****************************************
 *        Autor: Matteus Silvestre        *
 *        Aula: 01                        *
-*        TÌtulo: O comando printf()      *
+*        T√≠tulo: O comando printf()      *
 *        Data: 14/12/2018                *
 *****************************************/
 
 #include <stdio.h>
 
 void main(void){
-    /*O uso de identaÁ„o deixa o cÛdigo mais organizado
-    e È considerado boa pr·tica de programaÁ„o.*/
+    /*O uso de identa√ß√£o deixa o c√≥digo mais organizado
+    e √© considerado boa pr√°tica de programa√ß√£o.*/
+    
     //Os cinco printf() abaixo...
     printf("Hello");
     printf(" ");
     printf("Mundo");
     printf(" ");
     printf("Cruel");
-    /*...equivalem a printf("Hello Mundo Cruel") pois, por padr„o
-    a funÁ„o printf() n„o muda de linha. Uma nova linha È
-    representada pelo sÌmbolo \n */
+    /*...equivalem a printf("Hello Mundo Cruel") pois, por padr√£o
+    a fun√ß√£o printf() n√£o muda de linha. Uma nova linha √©
+    representada pelo s√≠mbolo \n */
     printf("\nHello\nMundo\nCruel\n");
-    /*A contrabarra È um caractere reservdado para tags do printf.
-    Para exibir a prÛpria contrabarra, basta usar duas contrabarras!*/
+    
+    /*A contrabarra √© um caractere reservdado para tags do printf.
+    Para exibir a pr√≥pria contrabarra, basta usar duas contrabarras!*/
     printf("\nContrabarra: \\");
     //Voltando ao que tentamos no comando inicial
     printf("\n\nHello\n\nMundo\n\nCruel\n\n");
-    //H· v·rias tags para o printf(). Por exemplo:
+    //H√° v√°rias tags para o printf(). Por exemplo:
     printf("\aBeep: \\a"); //A tag \a representa um "beep" sonoro
 }
