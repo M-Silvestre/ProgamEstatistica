d132e1dcbc92ca741eeba179fe76276747550780
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-01 10:18:46 -0200

    Update Aula02_Prog01.c

--- Aula02_Prog01.c
+++ Aula02_Prog01.c
@@ -1,14 +1,14 @@
 /*****************************************
 *        Autor: Matteus Silvestre        *
 *        Aula: 02                        *
-*        TÌtulo: IntroduÁ„o a Vari·veis  *
+*        T√≠tulo: Introdu√ß√£o a Vari√°veis  *
 *        Data: 14/12/2018                *
 *****************************************/
 
-/*Vari·vel È um espaÁo de memÛria usado para guardar e manipular
+/*Vari√°vel √© um espa√ßo de mem√≥ria usado para guardar e manipular
 determinado dado.
 
-Se define uma vari·vel assim:
+Se define uma vari√°vel assim:
 tipo nome_da_variavel;
 
 Para mais de uma do mesmo tipo:
@@ -20,43 +20,47 @@ int nota1, nota2, nota3;
 float altura, largura, comprimento;
 double taxa;
 
-Sendo cada tipo tem um propÛsito especÌfico:
+Existem quatro tipos b√°sicos, cada um com um prop√≥sito espec√≠fico:
     char: caracteres e strings
-    int: n˙meros inteiros (sem casas decimais)
-    float: n˙meros reais (com casas decimais)
-    double: n˙mero reais de maior precis„o
+    int: n√∫meros inteiros (sem casas decimais)
+    float: n√∫meros reais (com casas decimais)
+    double: n√∫mero reais de maior precis√£o
+    
+Existe um outro tipo de vari√°vel que pode ser considerado tipo b√°sico,
+o ponteiro, que indica um endere√ßo de mem√≥ria e ser√° abordadado em outro
+momento do curso.
 
-C aceita letras (padr„o ASCII), n˙meros e o _ (undescore) para 
-o nome de vari·veis, lembrando que a lingugem C È Case Sensitive.
-PorÈm, nomes de vari·vel inciados por n˙mero n„o s„o aceitos.
+C aceita letras (padr√£o ASCII), n√∫meros e o _ (undescore) para 
+o nome de vari√°veis, lembrando que a lingugem C √© Case Sensitive.
+Por√©m, nomes de vari√°vel inciados por n√∫mero n√£o s√£o aceitos.
 
-… possÌvel declarar vari·veis de tipos diferentes em uma mesma linha,
-de nome iniciado por _, ou declaradas fora da funÁ„o main(),
-mas essas n„o s„o boas pr·ticas de programaÁ„o.
+√â poss√≠vel declarar vari√°veis de tipos diferentes em uma mesma linha,
+de nome iniciado por _, ou declaradas fora da fun√ß√£o main(),
+mas essas n√£o s√£o boas pr√°ticas de programa√ß√£o.
 */
 
 #include <stdio.h>
 
 void main(void){
-    //… preciso declarar vari·veis antes de us·-las
-    int a, b; //"a" e "b" s„o espaÁos de memÛria diferentes
+    //√â preciso declarar vari√°veis antes de us√°-las
+    int a, b; //"a" e "b" s√£o espa√ßos de mem√≥ria diferentes
     
-    /*Na atribuiÁ„o de valores, a vari·vel que recebe o valor deve
-	estar ‡ esquerda e o valor, ‡ direita. TambÈm È possÌvel atribuir
-	um valor vindo de outra vari·vel, gerando uma cÛpia dele.*/ 
+    /*Na atribui√ß√£o de valores, a vari√°vel que recebe o valor deve
+	estar √† esquerda e o valor, √† direita. Tamb√©m √© poss√≠vel atribuir
+	um valor vindo de outra vari√°vel, gerando uma c√≥pia dele.*/ 
     a = 1; //"a" recebe o valor 1
     
-    /*O sÌmbolo % È usado em tags do printf para exibir valores de vari·veis.
-	Algumas delas s„o:
-    %d : n˙meros inteiros (formato decimal)
-    %f : n˙meros reais
+    /*O s√≠mbolo % √© usado em tags do printf para exibir valores de vari√°veis.
+	Algumas delas s√£o:
+    %d : n√∫meros inteiros (formato decimal)
+    %f : n√∫meros reais
     %c : caractere
     %s : string
-    A vari·vel a cujo valor ser· exibido deve ser especificada
+    A vari√°vel a cujo valor ser√° exibido deve ser especificada
     */
     printf("O valor de \"a\" eh %d\n\n", a);
     
-    b = a; //"b" recebe uma cÛpia do valor atual de a
-    a = 2; //"a" tem seu valor modificado, mas "b" n„o muda!
+    b = a; //"b" recebe uma c√≥pia do valor atual de a
+    a = 2; //"a" tem seu valor modificado, mas "b" n√£o muda!
     printf("O valor de \"a\" era %d e agora eh %d\n\n", b, a);
 }
