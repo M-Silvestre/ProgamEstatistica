accdc37ce759ba7bc1bafccbcfe8173debe44c41
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-08 14:18:58 -0300

    Update Aula01_Prog05.c

--- Aula01_Prog05.c
+++ Aula01_Prog05.c
@@ -13,16 +13,16 @@ void main(void){
     int mortalidade = 10, natalidade = 1000 ;
     float taxa;  
     
-    //Também é possível atribuir valores vindoes de operações entre variáveis
+    //Também é possível atribuir valores vindos de operações entre variáveis
     taxa = mortalidade/natalidade;
     printf("O valor da taxa eh %.2f\n\n", taxa);
     /*Por padrão, em C divisão de entre inteiros retorna inteiro
     (casas decimais são descartadas). 
     Ex: 9 dividido por 5 é igual 1.8, mas em C a divisão retornará 1*/
     
-    /*Para forçar o retorno  tipo float,
-    é preciso usar o "cast", que converte tipos*/
+    /*Para forçar o retorno tipo float, é preciso operar usando variáveis
+    tipo float ou usar o "cast", que converte tipos*/
     taxa = (float) mortalidade/natalidade;
     printf("O valor da taxa eh %.2f\n\n", taxa);
-    //O uso se %.2f em vez de %f limita o número de casas decimais para 2
+    //O uso se %.2f em vez de %f limita o número de casas decimais de 6 para 2
 }
