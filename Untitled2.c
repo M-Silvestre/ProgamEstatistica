d7c3d141bf0930175e484f8a9f23120237f4def9
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-08 12:59:02 -0200

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/*void main(void){
+    double Peso, Altura, IMC ;
+    printf("Calculo do IMC \n\n");
+    printf("Insira os dados relativos a peso e altura (separados por espaco): ");
+    scanf("%lf %lf", &Peso, &Altura);
+    IMC = (Peso)/(Altura*Altura);
+    printf("IMC : %.2f", IMC);
+
+}
+
+
+void main(void){
+    double Celsius, Fahrenheit;
+    printf("Transformador de temperatura (Celsius para Fahrenheit)\n\n");
+    printf("Informe uma temperatura em Celsius: ");
+    scanf("%lf", &Celsius);
+    Fahrenheit = Celsius * (9.0/5.0) + 32.0;
+    printf("%.2f graus Celsius equivale a %.2f graus Fahrenheit", Celsius, Fahrenheit);
+
+}
+
+
+void main(void){
+    double C, F;
+    printf("Transformador de temperatura (Fahrenheit para Celsius)\n\n");
+    printf("Informe uma temperatura em Fahrenheit: ");
+    scanf("%lf", &F);
+    C = (F - 32.0)*(5.0/9.0);
+    printf("%.2f graus Fahrenheit equivale a %.2f graus Celsius", F, C);
+
+}
+
+
+
+void main(void){
+    double R, G, pi = 3.1415926535 ;
+    printf("Transformador de Graus em Radianos  \n\n");
+    printf("informe o angulo em Graus : ");
+    scanf("%lf", &G);
+    R = G*(pi/180);
+    printf("%f Graus equivale a %f Radianos", G, R);
+
+}
+
+*/
+
+
+void main(void){
+    double salb, grat, imp, ded, valor;
+    /*salb - salário base
+    grat - gratificação
+    imp - imposto a ser informado
+    ded - dedução total
+    valor - valor final*/
+
+}
