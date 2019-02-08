3c3e2ed1cbd5e0167afdf06351da58045bb7ad06
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-08 11:47:59 -0200

    Update Aula02_Prog01.c

--- Aula02_Prog01.c
+++ Aula02_Prog01.c
@@ -9,17 +9,17 @@
 #include <math.h> //Biblioteca de funções matemáticas
 
 void main(void){
-    int num1, num2;
+    int num1, num2, soma;
     double raiz;
     num1 = 2;
     num2 = 6;
     soma = num1 + num2;
     raiz = sqrt(num1 + num2); //sqrt() vem de "square root", raiz quadrada
 
-    printf("A raiz quadra de (%d + %d) eh %.3f\n", num1, num2, raiz);
+    printf("A raiz quadrada de (%d + %d) eh %.3f\n", num1, num2, raiz);
     //printf aceita funções de variáveis no argumento
-    printf("A raiz quadra de (%d + %d) eh %.3f\n", num1, num2, sqrt(soma));
-    printf("A raiz quadra de (%d + %d) eh %.3f\n\n", num1, num2, sqrt(num1+num2));
+    printf("A raiz quadrada de (%d + %d) eh %.3f\n", num1, num2, sqrt(soma));
+    printf("A raiz quadrada de (%d + %d) eh %.3f\n\n", num1, num2, sqrt(num1+num2));
 
     //Lembrete que em C, operação entre inteiros sempre retorna inteiro!
     double div = num1/num2; //2 dividido por 6
