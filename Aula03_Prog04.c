bce76195d2363328cfc972e672c313d0c1c3b019
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-08 15:20:25 -0300

    Update Aula03_Prog04.c

--- Aula03_Prog04.c
+++ Aula03_Prog04.c
@@ -1,7 +1,7 @@
 /********************************************
 *        Autor: Matteus Silvestre           *
 *        Aula: 03                           *
-*        Título: Exercício 02               *
+*        Título: Exercício 02 (IMC)         *
 *        Data: 08/02/2019                   *
 *********************************************/
 
@@ -9,7 +9,7 @@
 
 void main(void){
     double Peso, Altura, IMC ;
-    printf("Calculo do IMC \n\n");
+    printf("Calculo do Indice de Massa Corporea\n\n");
     printf("Insira os dados relativos a peso e altura (separados por espaco): ");
     scanf("%lf %lf", &Peso, &Altura);
     IMC = (Peso)/(Altura*Altura);
