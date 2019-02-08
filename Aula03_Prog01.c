a99b11001080da7f0359ccbb5270f3271dbe5ca3
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-08 15:36:38 -0300

    Update Aula03_Prog01.c

--- Aula03_Prog01.c
+++ Aula03_Prog01.c
@@ -9,19 +9,21 @@
 
 void main(void){
     int a, b;
-    printf("\nDigite dois inteiros no formato (a b): ");
+    printf("\nDigite um inteiro: ");
     /*A função scanf() permite que o programa receba informações
     durante sua execução e as armazene em variáveis*/
-    
-    scanf("(%d %d)", &a, &b);
+    scanf("%d", &a);
+	
+    printf("\nDigite um outro inteiro: ");
+    scanf("%d", &b);
+	
     /*É importante que os dados sejam informados no mesmo formato que o scanf() os lerá,
-    Ex: separados por espaços, vírgulas, presença de parenteses ou hífen, etc.
-	  Caso contrário, valores incorretos podemser recebidos (como um espaço ou um Enter 
-	  no lugar de um número) e levar a resultados incoerentes.
+    Ex: inteiro, float ou char; presença de espaços ou vírgulas, parênteses ou hífen, etc.
+    Caso contrário, valores incorretos podem ser recebidos (como um espaço ou um Enter 
+    no lugar de um número) e levar a resultados incoerentes.
     
     O símbolo "&" siginifica endereço de memória, e no scanf() significa onde o
-	  valor recebido será armazenado. Neste caso, usar apenas o nome da variável 
-	  não é sufuciente!
-    */
-    printf("\n o conteudo de \"a\" eh %d, o valor de \"b\" eh %d\n", a, b);
+    valor recebido será armazenado. Neste caso, usar apenas o nome da variável 
+    não é sufuciente! */
+    printf("\nO conteudo de \"a\" eh %d, o valor de \"b\" eh %d\n", a, b);
 }
