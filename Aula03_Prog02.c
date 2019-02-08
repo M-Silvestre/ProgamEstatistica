5dd0ef2fe1c492910136dddc59ece9237801cd29
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-08 15:31:51 -0300

    Update Aula03_Prog02.c

--- Aula03_Prog02.c
+++ Aula03_Prog02.c
@@ -11,7 +11,7 @@ void main(void){
     long int a, b, c, d, resultado;
     printf("Informe quatro numeros inteiros separados po virgula: ");
     scanf("%d,%d,%d,%d", &a,&b,&c,&d);
-    //O scanf() pode receber vários valores de uma vez, desde que sejam
+    /*O scanf() pode receber vários valores de uma vez, desde que sejam
     separados e ordenados corretamente.*/
     resultado = (a + b + c)*d;
     printf("O resultado de (a+b+c)*d eh %ld", resultado);
