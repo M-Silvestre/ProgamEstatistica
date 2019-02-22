9bab13178ffa2296dbea9c8b1bc759bce84185aa
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-22 11:58:31 -0300

    Rename Exercicio_notacao_cientifica.c to 2-Exercicio_notacao_cientifica.c

--- Aula05/2-Exercicio_notacao_cientifica.c
+++ Aula05/2-Exercicio_notacao_cientifica.c
@@ -1,22 +1,22 @@
 /***********************************************
 *        Autor: Matteus Silvestre              *
 *        Aula: 05                              *
-*        Título: Exercícios(notação científica)*
+*        TÃ­tulo: ExercÃ­cios(notaÃ§Ã£o cientÃ­fica)*
 *        Data: 22/02/2019                      *
 ************************************************/
-/*Exercício 02: Escrever um programa que converta toneladas para
- quilos e gramas escrevendo o resultado em notação tradicional
- e científica.
+/*ExercÃ­cio 02: Escrever um programa que converta toneladas para
+ quilos e gramas escrevendo o resultado em notaÃ§Ã£o tradicional
+ e cientÃ­fica.
  
- 	Números reais representados em notação cientpifica seguem a forma
-	"aaaEbbb", onde o "E" significa 10, "aaa" é o número real entre 1 e 10 
+ 	NÃºmeros reais representados em notaÃ§Ã£o cientpifica seguem a forma
+	"aaaEbbb", onde o "E" significa 10, "aaa" Ã© o nÃºmero real entre 1 e 10 
 	e "bbb" o exponte. Por exemplo:
 	13000 viraria 1.3E04 (1.3 vezes 10 elevado a 4)
 	0.000098 viraria 9.8E-05 (9.8 vezes 10 elevado a -5)
 	
-	As tags do printf() e scanf() para números em notação científica
-	são %e e %E (dependendo se o "e" é maísculo ou não.
-	É possível representar no printf() com %E um valor recebido
+	As tags do printf() e scanf() para nÃºmeros em notaÃ§Ã£o cientÃ­fica
+	sÃ£o %e e %E (dependendo se o "e" Ã© maÃ­sculo ou nÃ£o.
+	Ã‰ possÃ­vel representar no printf() com %E um valor recebido
 	pelo scanf() com %f, e vice-versa.*/
 
 #include <stdio.h>
@@ -27,9 +27,9 @@ void main(void){
 	printf("Conversao de massa\n\n");
 	printf("Informe a massa (em toneladas): ");
 	scanf("%lf", &massaT); //Recebendo como float
-	//scanf("%e", &massaT); Rebendo como notação científica
-	massaQ = massaT * 1.0e03; //1.0e03 é 1 vezes 10 elevado a 3, ou 1000
-	massaG = massaT * 1.0e06; //1.0e03 é 1 vezes 10 elevado a 3, ou 1000000
+	//scanf("%e", &massaT); Rebendo como notaÃ§Ã£o cientÃ­fica
+	massaQ = massaT * 1.0e03; //1.0e03 Ã© 1 vezes 10 elevado a 3, ou 1000
+	massaG = massaT * 1.0e06; //1.0e03 Ã© 1 vezes 10 elevado a 3, ou 1000000
 
 	printf("\n%.2f toneladas equivale a:\n"
 			"\t%.2E ou %.2f quilogramas"
