10698887fefa6cc369c2e978e4165eddf9ca284d
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-22 12:00:24 -0300

    Update and rename Exemplo_caractere_newline.c to 3-Caracteres_e_newline.c

--- Aula05/3-Caracteres_e_newline.c
+++ Aula05/3-Caracteres_e_newline.c
@@ -1,17 +1,18 @@
 /***********************************************
 *        Autor: Matteus Silvestre              *
 *        Aula: 05                              *
-*        Título: Caracteres                     *
+*        TÃ­tulo: Caracteres e newline          *
 *        Data: 22/02/2019                      *
 ************************************************/
 
 #include <stdio.h>
 
 void main(void){
-	//O "new line", ou pular linha, é um único caractere ASCII
+	//O "new line", ou pular linha, Ã© um Ãºnico caractere ASCII
 	char caractere = '\n';
 	printf("%c%c%c", caractere, caractere, caractere);
 	
+	//Como em int, o % pode indicar valores nÃ£o vindos de uma variÃ¡vel
 	printf("%cello %cund%c %c%cue%c\n", 'H', 'M', 'o', 'C', 'r', 'l');
 		
 	system("pause");
