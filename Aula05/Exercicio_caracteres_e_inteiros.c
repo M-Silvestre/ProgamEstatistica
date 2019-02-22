ab33defb1138f76f3b6be39c2c09f373e172b8bd
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-22 11:32:42 -0300

    Update Exercicio_caracteres_e_inteiros.c

--- Aula05/Exercicio_caracteres_e_inteiros.c
+++ Aula05/Exercicio_caracteres_e_inteiros.c
@@ -13,16 +13,26 @@
 
 void main(void){
 	printf("Relacao entre caracteres e inteiros pela tabela ASCII\n\n");
-	int ch;
+	int num;
 	printf("Inteiro para caractere\n"
 			"\tDigite um inteiro: ");
-	scanf("%d", &ch);
-	printf("\tO inteiro %d equivale a %c na tabela ASCII\n\n", ch, ch);
-	
+	scanf("%d", &num);
 	fflush(stdin);
+	printf("\tO inteiro %d equivale a '%c' na tabela ASCII\n\n", num, num);
+	/*Ocorreu o chamado "casting" automático, ou seja, a conversão de tipos
+	feita pela máquina (int para char)*/
+	
+	char ch;
 	printf("Caractere para inteiro\n"
 			"\tDigite um caractere: ");
+	/*Inteiros menores que 0 ou maiores que 255 não  da tabela ASCII,
+	por isso serão associados a caracteres fora da tabela*/
 	scanf("%c", &ch);
-	printf("\tO caractere %c equivale a %d na tabela ASCII\n\n", ch, ch);
+	printf("\tO caractere '%c' equivale a %d na tabela ASCII\n\n", ch, (int) ch);
+	/*Já aqui ocorreu "casting" não automático, ou seja, a conversão explicitade
+	(char para int)
+	O casting não automático é considerado boa prática de programação, pois
+	explicita conversões de tipo no código. Além disso, algumas conversões não
+	podem ser feitas automaticamente, como inteiro para real.*/
 	system("pause");
 }
