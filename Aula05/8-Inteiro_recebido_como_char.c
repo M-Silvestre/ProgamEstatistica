a5fa56733d16edbd1a5b032fdb722824161375d4
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-22 12:57:11 -0300

    Create 8-Inteiro_recebido_como_char.c

---
+++ Aula05/8-Inteiro_recebido_como_char.c
@@ -0,0 +1,23 @@
+/*******************************************
+*        Autor: Matteus Silvestre          *
+*        Aula: 05                          *
+*        Título: Caract e inteiros (Obs)  *
+*        Data: 22/02/2019                  *
+********************************************/
+
+/*Observação: Apesar de ser possível converter int em char e vice-versa,
+ler um int usando %c ou um char usando %d pode levar a comportamentos imprevisíveis.*/
+
+#include <stdio.h>
+
+void main(void){
+	int num = 1000; //Valor inteiro, 00000011 11101000 em binário (ocupa 2 bytes)
+	printf("Digite um caractere: ");
+	scanf("%c", &num);
+	/*Variáveis char ocupam apenas um byte. Logo, scanf() vai receber apenas um byte.
+	Ou seja, apenas o segundo byte do inteiro é alterado!*/
+	
+	printf("O inteiro num = %d representa '%c' na tabela ASCII\n\n", num, (char) num);
+	//Observe que inteiro não corresponde ao caractere!
+	system("pause");
+}
