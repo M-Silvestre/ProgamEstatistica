bef3f771d7b8aa85279297472966616c809a098a
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-03-01 18:09:23 -0300

    Rename 6-Modificando_x_usando_x.c to 06-Modificando_x_usando_x.c

--- Aula06/06-Modificando_x_usando_x.c
+++ Aula06/06-Modificando_x_usando_x.c
@@ -1,10 +1,10 @@
 /*****************************************
 *        Autor: Matteus Silvestre        *
 *        Aula: 06                        *
-*        Título: Mudando X usando X *
+*        TÃ­tulo: Mudando X usando X *
 *        Data: 01/03/2019                *
 ******************************************/
-/*	Exercício: Implemente um programa que recebe um valor de
+/*	ExercÃ­cio: Implemente um programa que recebe um valor de
 	salario e, se o salario for menor que 100000, adiciona 1000
 	*/
 
@@ -14,13 +14,13 @@
 void main(void){
 	double salario;
 	printf("Informe um sal%crio: ", 160);
-	//O inteiro 160 corresponde ao caractere 'á'
+	//O inteiro 160 corresponde ao caractere 'Ã¡'
 	scanf("%lf", &salario);
-	if(salario < 1e05){ //salario é menor que 100000?
+	if(salario < 1e05){ //salario Ã© menor que 100000?
 		salario = salario + 1e03;
-		/*C permite instanciar um novo valor para uma variável
+		/*C permite instanciar um novo valor para uma variÃ¡vel
 		usando seu valor anterior. Por exemplo, se temos um x = 5,
-		podemos usar x = x + 1, e então x será igual a 6*/ 
+		podemos usar x = x + 1, e entÃ£o x serÃ¡ igual a 6*/ 
 		printf("\nO novo sal%crio %c R$ %.2f", 160, 130, salario);
 	} else{
 		printf("\nN%co houve alterac%co no sal%crio de R$ %.2f", 198, 198, 130, salario);
