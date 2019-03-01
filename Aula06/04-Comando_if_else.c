85cf8b3c034d0da95e33ecdd01c460311b6de5db
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-03-01 18:05:40 -0300

    Rename 4-Comando_if_else.c to 04-Comando_if_else.c

--- Aula06/04-Comando_if_else.c
+++ Aula06/04-Comando_if_else.c
@@ -1,26 +1,26 @@
 /***********************************
 *        Autor: Matteus Silvestre  *
 *        Aula: 06                  *
-*        Título: Comando if() else *
+*        TÃ­tulo: Comando if() else *
 *        Data: 01/03/2019          *
 ************************************/
 
 /*
 	Um dos comandos usados para controlar o fluxo do do programa
-	é o if() else. O nome vem do inglês "if", que significa "se",
-	e "else", "caso contrário". A sintaxe do comando é:
+	Ã© o if() else. O nome vem do inglÃªs "if", que significa "se",
+	e "else", "caso contrÃ¡rio". A sintaxe do comando Ã©:
 	
-	if(expressão lógica){
+	if(expressÃ£o lÃ³gica){
 		comandos a serem executados se
-		a expressão é verdadeira (retorna valor
+		a expressÃ£o Ã© verdadeira (retorna valor
 		diferente de zero)
 	} else{
 		comandos a serem executados se
-		a expressão é falsa (retorna zero)
+		a expressÃ£o Ã© falsa (retorna zero)
 	}
 
-	Exercício: Implemente um programa que receba um inteiro e
-	infor se ele é não negativo ou negativo
+	ExercÃ­cio: Implemente um programa que receba um inteiro e
+	infor se ele Ã© nÃ£o negativo ou negativo
 
 */
 
@@ -30,13 +30,13 @@
 void main(void){
 	int num;
 	printf("Informe um n%c inteiro: ", 167);
-	//O inteiro 167 corresponde ao caractre 'º'
+	//O inteiro 167 corresponde ao caractre 'Âº'
 	scanf("%d", &num);
 	if(num >= 0){
 		printf("O n%cmero %d %c n%co negativo.", 163, num, 130, 198);
-		//O inteiro 163 corresponde ao caractere 'ú'
-		//O inteiro 130 corresponde ao caractere 'é'
-		//O inteiro 198 corresponde ao caractere 'ã'
+		//O inteiro 163 corresponde ao caractere 'Ãº'
+		//O inteiro 130 corresponde ao caractere 'Ã©'
+		//O inteiro 198 corresponde ao caractere 'Ã£'
 	} else{
 		printf("O n%cmero %d e negativo", 163, num);
 	};
