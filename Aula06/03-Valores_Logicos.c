18c88067cdd069d5332e944ba91d09cc27b053d7
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-03-01 18:06:58 -0300

    Rename 3-Valores_Logicos.c to 03-Valores_Logicos.c

--- Aula06/03-Valores_Logicos.c
+++ Aula06/03-Valores_Logicos.c
@@ -1,28 +1,28 @@
 /*****************************************************
 *        Autor: Matteus Silvestre                    *
 *        Aula: 06                                    *
-*        Título: Valores Lógivos: Verdadeiro e Falso *
+*        TÃ­tulo: Valores LÃ³givos: Verdadeiro e Falso *
 *        Data: 01/03/2019                            *
 ******************************************************/
 /*
-	Como vimos antes, C possui quatro tipos básicos (int, float,
+	Como vimos antes, C possui quatro tipos bÃ¡sicos (int, float,
 	double e char) e o tipo ponteiro.
 	
-	Porém, não existe em C um tipo específico para representar os 
-	valores lógicos "Verdadeiro" e "Falso", diferentemente de C++
+	PorÃ©m, nÃ£o existe em C um tipo especÃ­fico para representar os 
+	valores lÃ³gicos "Verdadeiro" e "Falso", diferentemente de C++
 	e outras linguagens como R, Java, etc.
-	Em vez disso, por padrão C interpreta o número zero como
-	"falso" e qualquer número diferente de zero (int ou não) como
-	verdadeiro. Os operadores relacionais em C são:
+	Em vez disso, por padrÃ£o C interpreta o nÃºmero zero como
+	"falso" e qualquer nÃºmero diferente de zero (int ou nÃ£o) como
+	verdadeiro. Os operadores relacionais em C sÃ£o:
 	
 	== : Igualdade 
 	>  : Maior que
 	>= : Maior ou igual a
 	<  : Menor que
 	<= : Menor ou igual
-	!= : Diferença
+	!= : DiferenÃ§a
 	
-	Expressões que usam esses operadores retornam 0 quando falsas,
+	ExpressÃµes que usam esses operadores retornam 0 quando falsas,
 	e 1 quando verdadeiras
 */
 
@@ -37,14 +37,14 @@ void main(void){
 	scanf("%d", &Y);
 	printf("\nOperacoes Relacionais (0 : Falso, 1 : Verdadeiro)\n");
 	printf("\n\tX e igual a Y? %d", (X == Y));
-	/*OBS1: Não confundir o operador relacional ==
-	e o operador de atribuição =*/
+	/*OBS1: NÃ£o confundir o operador relacional ==
+	e o operador de atribuiÃ§Ã£o =*/
 	printf("\n\tX e maior que Y? %d", (X > Y));
 	printf("\n\tX e maior ou igual a Y? %d", (X >= Y));
 	printf("\n\tX e menor que Y? %d", (X < Y));
 	printf("\n\tX e menor ou igual a Y? %d", (X <= Y));
 	printf("\n\tX e diferente de Y? %d\n\n", (X != Y));
-	/*OBS2: É possível usar os operadores para comparar
-	expressões, como por exemplo (x + y) <= (Y - X*Y - 4)*/
+	/*OBS2: Ã‰ possÃ­vel usar os operadores para comparar
+	expressÃµes, como por exemplo (x + y) <= (Y - X*Y - 4)*/
 	system("pause");
 }
