e0f269089aa38144c8e362c61caab0338df0cb2c
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-04-25 14:14:50 -0300

    Create 1-Biblioteca.h

---
+++ Variaveis_Aleatorias/1-Biblioteca.h
@@ -0,0 +1,37 @@
+#include <math.h> //Precisaremos da função pow(), para potências
+
+/*OBS: É possível criar uma função que usa outra, desde que a a função de dentro
+	seja declarada antes da função maior. Veja:
+*/
+
+//Primeira função: retorna o fatorial de um número dado
+int fat(int y){
+  int i, f;
+	f = 1;
+	//Vamos fazer o produto de i * (i - 1) * (i - 2)... = i!
+	for(i = y; i > 1; i--){
+		f *= i;
+	};
+	return f;
+};
+
+//Segunda função: retorna a combinação de n para escolher x (usa o fatorial)
+int comb(int n, int x){
+	int c;
+	c = fat(n)/(fat(x)*fat(n-x));
+	return c;
+};
+
+//Terfeira função: retorna a probabilidade de uma v.a. binomial (usa a combinação)
+float bin(int n, float p, int x){
+	float prob;
+	prob = 100 * comb(n, x) * pow(p, x) * pow(1- p, n - x);
+	return prob; //OBS: Retorna como percentual: 0 <= prob <= 100
+};
+
+
+float geo(float p, int x){
+	float px;
+	px = 100* pow(1 - p, x - 1) * p;
+	return px;
+}
