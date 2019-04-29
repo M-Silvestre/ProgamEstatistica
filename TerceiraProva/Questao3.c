70b70b08e29c5ecbe4427d3b25a119728b97fc1e
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-04-29 18:56:17 -0300

    Update Questao3.c

--- TerceiraProva/Questao3.c
+++ TerceiraProva/Questao3.c
@@ -5,13 +5,14 @@
 *        Data: 26/04/2019                            *
 ******************************************************/
 
-/*	Questão 3: Crie uma função que que retorne o fatorial
+/*	
+	Questão 3: Crie uma função que que retorne o fatorial
 	de um número natural.
 */
 
 #include <stdio.h>
 
-int fat(int n);
+int fat(int n), fat_r(int n);
 
 void main(void){
 	printf("Fatorial de um n%cmero\n\n", 163);
@@ -19,11 +20,25 @@ void main(void){
 	printf("Informe um n%cmero natural: ", 163);
 	scanf("%d", &n);
 	printf("\n%d! = %d", n, fat(n));
+	printf("\n%d! = %d", n, fat_r(n));
 	
 };
 
 //Resposta:
-int fat(n){
+
+//De forma não recursiva:
+int fat(int n){
+	if(n == 0 || n == 1)
+		return 1;
+	int i;	
+	int res = 1;
+	for(i = n; i > 1; i--)
+		res *= i;
+	return res;
+};
+
+//De forma recursiva
+int fat_r(int n){
 	if(n ==0 || n == 1)
 		return 1;
 	else
