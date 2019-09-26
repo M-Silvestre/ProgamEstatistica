e5ca6e9bfea29945af0ea8ee44d8d123a3b22892
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-09-26 16:05:52 -0300

    Update 03-Valores_Logicos.c

--- Aula06/03-Valores_Logicos.c
+++ Aula06/03-Valores_Logicos.c
@@ -1,18 +1,18 @@
 /*****************************************************
 *        Autor: Matteus Silvestre                    *
 *        Aula: 06                                    *
-*        Título: Valores Lógivos: Verdadeiro e Falso *
+*        Título: Valores Lógicos: Verdadeiro e Falso *
 *        Data: 01/03/2019                            *
 ******************************************************/
 /*
-	Como vimos antes, C possui quatro tipos básicos (int, float,
-	double e char) e o tipo ponteiro.
+	Como vimos antes, C possui quatro tipos básicos de variáveis
+	(int, float, double e char) e o tipo ponteiro.
 	
 	Porém, não existe em C um tipo específico para representar os 
 	valores lógicos "Verdadeiro" e "Falso", diferentemente de C++
 	e outras linguagens como R, Java, etc.
 	Em vez disso, por padrão C interpreta o número zero como
-	"falso" e qualquer número diferente de zero (int ou não) como
+	"falso" e qualquer valor diferente de zero (int ou não) como
 	verdadeiro. Os operadores relacionais em C são:
 	
 	== : Igualdade 
@@ -38,7 +38,7 @@ void main(void){
 	printf("\nOperacoes Relacionais (0 : Falso, 1 : Verdadeiro)\n");
 	printf("\n\tX e igual a Y? %d", (X == Y));
 	/*OBS1: Não confundir o operador relacional ==
-	e o operador de atribuição =*/
+	e o operador de atribuição = */
 	printf("\n\tX e maior que Y? %d", (X > Y));
 	printf("\n\tX e maior ou igual a Y? %d", (X >= Y));
 	printf("\n\tX e menor que Y? %d", (X < Y));
